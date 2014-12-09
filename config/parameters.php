4c39cded856d87090f2183b0dcd2478198b674a1
Andrey Mikheychik <a.mikheychik@gmail.com>
2014-12-09 16:03:27 -0500

    Configuration files moved out to installer plugin

--- config/parameters.php
+++
@@ -1,9 +0,0 @@
-<?php require_once __DIR__ . '/../vendor/autoload.php';
-
-use CDD\Drupal\DrupalConfigReader;
-
-include_once __DIR__ . '/config.php';
-include_once __DIR__ . '/environment.php';
-
-$configReader = new DrupalConfigReader($environment_name, $config, $parameters);
-$configReader->read();
